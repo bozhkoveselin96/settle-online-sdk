1a7216ad0e9b6bcf1ad1da3e2875cffb126d62c5
bozhkoveselin <veselin.bozhkov@infinno.eu>
2021-02-12 13:43:46 +0200

    fixed untracked files

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('MERCHANT_ID', '1');
-define('USER_ID', '');
-define('SECRET', '');
-define('STATUS_PENDING', 'PENDING');
-define('STATUS_SUCCESS', 'SUCCESS');
-define('STATUS_FAIL', 'FAIL');
-define('BASE_URL', '');
