68e97d103280e774f586165671d8704a1ce5e361
bozhkoveselin <veselin.bozhkov@infinno.eu>
2021-02-15 16:20:16 +0200

    update configuration file

--- src/config-copy.php
+++ src/config-copy.php
@@ -5,5 +5,5 @@ define('USER_ID', '');
 define('SECRET', '');
 define('STATUS_PENDING', 'PENDING');
 define('STATUS_SUCCESS', 'SUCCESS');
-define('STATUS_FAIL', 'FAIL');
+define('STATUS_FAIL', 'FAILED');
 define('BASE_URL', '');
